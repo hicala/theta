2ac0a26e41d9e7512f0ba5190879e41c4300820c
gartlar <root@kira.ibm.com>
2014-07-29 17:27:39 -0300

    Migrated to python

---
+++ backup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+# Custom settings.-
+path = "/opt/SystemBackup"
+name = path + "/data.xml"
+
+# Load packages.-
+import xml.etree.ElementTree as ET
+import os
+import sys
+sys.path.append('/opt/SystemBackup')
+from models import * 
+os.system('clear')
+
+# Load XML to memory.-
+tree = ET.parse(name)
+root = tree.getroot()
+
+# Load handlers.-
+HANDLERS = {}
+for h in root.findall('handlers')[0].findall('h'):
+	try: 
+		name = h.attrib['name']
+		b    = Backup()
+		b.setType(h.attrib['type'].upper())
+		b.setDir(h.attrib['location'])
+		if h.attrib['password']:
+			b.compress(h.attrib['password'])
+		HANDLERS[name] = b
+	except Exception as e:
+		print "Error while loading %s!! " % name 
+		print e
+
+# Load sources.-
+SOURCES = []
+for s in root.findall('source'):
+	dict = { 'handlers':[], 'files':[], }	
+	for h in s.findall('handler'):
+		dict['handlers'].append(h.text)
+	for f in s.findall('dir'):
+		dict['files'].append(f.text)
+	SOURCES.append(dict)
+
+# Combine Handlers and Sources and create a backup.-
+for s in SOURCES:
+
+	# For each handler.-
+	for h in s['handlers']:
+		if h in HANDLERS.keys():
+			backup = HANDLERS[h]
+		
+			# For each file.-
+			backup.clear()
+			for f in s['files']:
+				backup.add(f)
+
+			# Compile and run.-
+			backup.compile()
+			backup.run()
+
+			# End.-
+			print backup.NAME
+				
---
+++ models.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+from datetime import * 
+import os
+import sys
+import subprocess
+import re
+
+# Backup class.-
+# Example:
+# b = Backup()
+# b.setType("LOCAL")
+# b.setDir('/backup')
+# b.compress('a')
+# b.add('/tmp/bootstrap') 
+# b.compile()
+# b.run()
+class Backup:
+
+	# Available backup types.-
+	BACKUP_TYPES = ["LOCAL","MOUNT","FTP",]
+	
+	# Simple init method with empty elements.-
+	# TYPE	  = Type of backup among ("LOCAL","MOUNT","FTP")
+	# PASW 	  = Password for the compressed file.-
+	# TS   	  = Timestamp.-
+	# DIR	  = Destiny directory.-
+	# FILES   = List of files to be compressed.-
+	# CMD	  = Command to be executed.-
+	# NAME    = Full name of the compressed file.-
+	def __init__(self):
+		self.PASW  	= None
+		self.TYPE	= "LOCAL"
+		self.DIR	= "/tmp"
+		self.TS		= datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+		self.FILES 	= []
+		self.CMD   	= ""
+
+	# Checks File System usage
+	def df(self):
+		df = subprocess.Popen(["df","-HP",self.DIR], stdout=subprocess.PIPE)
+		output = df.communicate()[0]
+		arr  = output.split("\n")[1].split()	
+		size = arr[4]
+		# print "Usage: %s" % size
+		size = re.sub('%','',size) 
+		return float(size)
+
+	# Compiles files into self.CMD.-
+	# None self.compile()
+	def compile(self):
+		if len(self.FILES)>0:
+			self.NAME = "%s/backup-%s.zip" % (self.DIR,self.TS)
+			files = ' '.join(self.FILES)	
+			if self.PASW: 	pasw = "-P '%s'" % self.PASW
+			else:		pasw = ""
+			self.CMD = "zip -r %s %s %s" % (self.NAME,pasw,files)
+			print "Command: %s " % self.CMD 
+			return self.CMD 
+		else:
+			return ""
+
+	# Runs the self.CMD command
+	# void self.run()
+	def run(self):
+		if not self.CMD:
+			raise Exception("Need compilation first!")
+		else:
+			os.system(self.CMD)
+
+	# Adds a file to the self.FILES array.-
+	# void self.add(file)
+	def add(self,file):
+		if not os.path.exists(file):
+			raise Exception("File %s does not exist" % file)
+		elif not os.access(file, os.R_OK):
+			raise Exception("Can not read file %s" % file)
+		else:
+			self.FILES.append(file)
+
+	# Remove a file from the self.FILEs array.-
+	# void self.remove(file)
+	def remove(self,file):
+		self.FILES.remove(file)
+
+	# This adds a password to the zip command.-
+	# void self.compress(password)
+	def compress(self,pasword):
+		self.PASW = pasword
+
+	# This remove the password from the zip command.-
+	# void self.uncompress()
+	def uncompress(self):
+		self.PASW = None
+
+	# Set the zip destination.-
+	# void setDir(d)
+	def setDir(self,d):
+		self.DIR = d 
+		if not os.path.exists(self.DIR):
+			raise Exception("%s does not exist" % self.DIR)
+		elif os.path.isfile(self.DIR):
+			raise Exception("%s is not " % self.DIR)
+		elif not os.access(self.DIR, os.W_OK):
+			raise Exception("Can not write to %s" % self.DIR)
+		elif self.df()>70:
+			raise Exception("File System %s is above 70" % self.DIR)
+
+	# Set the backup type.-
+	# void setType(t)
+	def setType(self,t):
+		if t in self.BACKUP_TYPES:
+			self.TYPE = t
+		else:
+			raise Exception("Type %s is not supported " % t)
+
+	# Clear all files in this backup
+	# void clear()
+	def clear(self):
+		self.FILES = []
+
